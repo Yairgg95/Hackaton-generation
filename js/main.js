1469a5a888ea4239113647f5be47eaa637e74eb8
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-03-03 11:38:25 -0600

    [First commit]Add first structure

---
+++ js/main.js
@@ -0,0 +1,98 @@
+// Products data
+const tendenciesProducts = [
+  {
+    title: "CR7 snekears",
+    description: "Sneakers oficiales del SIIIIUUUU",
+    img: "sustituir por una liga de una imagen del producto",
+    quantityStock: 1,
+    price: 1500,
+    categories: ["Footbal", "Calzado"],
+    brand: "Nike"
+  },
+  {
+    title: "CR7 snekears",
+    description: "Sneakers oficiales del SIIIIUUUU",
+    img: "sustituir por una liga de una imagen del producto",
+    quantityStock: 1,
+    price: 1500,
+    categories: ["Footbal", "Calzado"],
+    brand: "Nike"
+  },
+  {
+    title: "CR7 snekears",
+    description: "Sneakers oficiales del SIIIIUUUU",
+    img: "sustituir por una liga de una imagen del producto",
+    quantityStock: 1,
+    price: 1500,
+    categories: ["Footbal", "Calzado"],
+    brand: "Nike"
+  },
+  {
+    title: "CR7 snekears",
+    description: "Sneakers oficiales del SIIIIUUUU",
+    img: "sustituir por una liga de una imagen del producto",
+    quantityStock: 1,
+    price: 1500,
+    categories: ["Footbal", "Calzado"],
+    brand: "Nike"
+  }
+  //Add more
+];
+
+
+const products = [
+    {
+      title: "CR7 snekears",
+      description: "Sneakers oficiales del SIIIIUUUU",
+      img: "sustituir por una liga de una imagen del producto",
+      quantityStock: 1,
+      price: 1500,
+      categories: ["Footbal", "Calzado"],
+      brand: "Adiddas"
+    },
+    {
+      title: "CR7 snekears",
+      description: "Sneakers oficiales del SIIIIUUUU",
+      img: "sustituir por una liga de una imagen del producto",
+      quantityStock: 1,
+      price: 1500,
+      categories: ["Footbal", "Calzado"],
+      brand: "Nike"
+    },
+    {
+      title: "CR7 snekears",
+      description: "Sneakers oficiales del SIIIIUUUU",
+      img: "sustituir por una liga de una imagen del producto",
+      quantityStock: 1,
+      price: 1500,
+      categories: ["Footbal", "Calzado"],
+      brand: "Nike"
+    },
+    {
+      title: "CR7 snekears",
+      description: "Sneakers oficiales del SIIIIUUUU",
+      img: "sustituir por una liga de una imagen del producto",
+      quantityStock: 1,
+      price: 1500,
+      categories: ["Footbal", "Calzado"],
+      brand: "Nike"
+    }
+    //Add more
+  ];
+// JavaScript render logic
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Fetch Blog content
+    const blogContent = () => {
+        try {
+          const res = fetch();  
+        } catch (error) {
+            throw new Error (error);
+        }
+
+    }
+    
+
+
+
+});
